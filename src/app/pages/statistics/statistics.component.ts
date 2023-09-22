a4d1b739c30f20af7febcbf9fd05e0e454637aef
poiuylkkk <1213512547@qq.com>
2023-09-22 22:21:10 +0800

    update statistics

--- src/app/pages/statistics/statistics.component.ts
+++ src/app/pages/statistics/statistics.component.ts
@@ -3,6 +3,7 @@ import * as echarts from 'echarts';
 import { NzTabChangeEvent } from 'ng-zorro-antd/tabs';
 import {fromEvent, Subject} from "rxjs";
 import {debounceTime, takeUntil} from "rxjs/operators";
+import { StatisticsService } from 'src/app/services/statistics.service';
 import {EchartsService} from "../../services/echarts.service";
 
 @Component({
@@ -24,6 +25,7 @@ export class StatisticsComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<void>();
   constructor(
     private echartsService: EchartsService,
+    private statisticsService: StatisticsService
   ) { }
 
   ngOnInit(): void {
@@ -130,7 +132,7 @@ export class StatisticsComponent implements OnInit, OnDestroy {
           emphasis: {
             focus: 'series'
           },
-          data: [820, 832, 901, 934, 1290, 1330, 1320]
+          data: [820, 832, 901, 934, 1290, 1330, 951]
         },
         {
           name: 'Financial',
@@ -273,43 +275,60 @@ export class StatisticsComponent implements OnInit, OnDestroy {
     this.echartsService.setEchartsOption(this.echartsContainerMid, option);
   }
 
-  getOption4() { // 柱状图
-    const option = {
-      dataset: [
-        {
-          dimensions: ['name', 'age', 'profession', 'score', 'date'],
-          source: [
-            ['Hannah Krause', 41, 'Engineer', 314, '2011-02-12'],
-            ['Zhao Qian', 20, 'Teacher', 351, '2011-03-01'],
-            ['Jasmin Krause ', 52, 'Musician', 287, '2011-02-14'],
-            ['Li Lei', 37, 'Teacher', 219, '2011-02-18'],
-            ['Karle Neumann', 25, 'Engineer', 253, '2011-04-02'],
-            ['Adrian Groß', 19, 'Teacher', '-', '2011-01-16'],
-            ['Mia Neumann', 71, 'Engineer', 165, '2011-03-19'],
-            ['Böhm Fuchs', 36, 'Musician', 318, '2011-02-24'],
-            ['Han Meimei', 67, 'Engineer', 366, '2011-03-12']
-          ]
+  getOption4() {
+    this.statisticsService.getNetData().subscribe((graph: any) => {
+      graph.nodes.forEach((node: any) => {
+        node.label = {
+          show: node.symbolSize > 30
+        };
+      });
+      const option = {
+        title: {
+          text: '',
+          subtext: 'Default layout',
+          top: 'bottom',
+          left: 'right'
         },
-        {
-          transform: {
-            type: 'sort',
-            config: { dimension: 'score', order: 'desc' }
+        tooltip: {},
+        legend: [
+          {
+            // selectedMode: 'single',
+            data: graph.categories.map(function (a: any) {
+              return a.name;
+            })
           }
-        }
-      ],
-      xAxis: {
-        type: 'category',
-        axisLabel: { interval: 0, rotate: 30 }
-      },
-      yAxis: {},
-      series: {
-        type: 'bar',
-        encode: { x: 'name', y: 'score' },
-        datasetIndex: 1
-      }
-    };
+        ],
+        animationDuration: 1500,
+        animationEasingUpdate: 'quinticInOut',
+        series: [
+          {
+            name: '',
+            type: 'graph',
+            layout: 'none',
+            data: graph.nodes,
+            links: graph.links,
+            categories: graph.categories,
+            roam: true,
+            label: {
+              position: 'right',
+              formatter: '{b}'
+            },
+            lineStyle: {
+              color: 'source',
+              curveness: 0.3
+            },
+            emphasis: {
+              focus: 'adjacency',
+              lineStyle: {
+                width: 10
+              }
+            }
+          }
+        ]
+      };
+      this.echartsService.setEchartsOption(this.echartsContainerRight, option);
+    });
 
-    this.echartsService.setEchartsOption(this.echartsContainerRight, option);
   }
 
 
